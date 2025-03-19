9c78f2d836409dc4ec2d08af230b067b1593bb32
FuadHmdn <fuadhamidan.drive@gmail.com>
2025-03-19 12:19:13 +0700

    create api config

--- app/src/main/java/com/fuad/sinus/data/remote/ApiConfig.kt
+++ app/src/main/java/com/fuad/sinus/data/remote/ApiConfig.kt
@@ -1,4 +1,28 @@
 package com.fuad.sinus.data.remote
 
+import com.fuad.sinus.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ApiConfig {
+
+    fun getApiService(): ApiService {
+
+        val httpLoggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
 }
--- app/src/main/java/com/fuad/sinus/data/remote/ApiService.kt
+++ app/src/main/java/com/fuad/sinus/data/remote/ApiService.kt
@@ -1,4 +1,14 @@
 package com.fuad.sinus.data.remote
 
+import com.fuad.sinus.data.remote.response.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ApiService {
+
+    @GET("publik/prakiraan-cuaca")
+    suspend fun getWeather(
+        @Query("adm4") kodeWilayah: String
+    ): Response
+
 }
